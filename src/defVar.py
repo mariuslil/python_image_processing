1abe9f1f661617bb2b52ef73b9933bafaf6a2f63
marlil <marlil@stud.ntnu.no>
2018-04-17 09:15:32 +0200

    gjort mer på inpainting

--- src/defVar.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-pause = 1e-4
-alpha = .25
-steps = 10000
-im = plt.imread('lena.png')
-im = np.sum(im, 2) / 3
--- src/inpainting.py
+++ src/inpainting.py
@@ -1,24 +1,41 @@
-from defVar import *    #Import variabels and packages
+import numpy as np
+import matplotlib.pyplot as plt
 
+pause = 1e-4
+alpha = .25
+steps = 10000
+im = plt.imread('lena.png')
+im = np.sum(im, 2) / 3
 
-mask = np.zeros(im.shape)
-mask[200:250, 0:200] = 255
-mask[360:380, 140:185] = 255
-mask[400:500, 270:345] = 255
+def explicitLaplace(img):
+    return (img[:-2, 1:-1] +
+            img[2:, 1:-1] +
+            img[1:-1, :-2] +
+            img[1:-1, 2:] -
+            4*img[1:-1, 1:-1])
+
+mask0 = np.zeros(im.shape)
+mask0[360:370, 140:150] = 255
+
+mask1 = np.zeros(im.shape)
+mask1[245:255, 270:280] = 255
 
 broken = im.copy()
-broken[np.where(mask)] = 1
+broken[np.where(mask0)] = 1
+broken[np.where(mask1)] = 1
 
+imTemp = broken[359:371, 139:151]
 
+for i in range(0, steps):
+    imTemp[1:-1, 1:-1] += alpha * explicitLaplace(imTemp)
 
+imTemp = broken[244:256, 269:281]
 
-while True:
-    plt.ion()
-    data = plt.imshow(broken, plt.cm.gray)
-    data.set_array(broken)
-    plt.draw()
-    plt.pause(pause)
+for i in range(0, steps):
+    imTemp[1:-1, 1:-1] += alpha * explicitLaplace(imTemp)
 
-
-
-#blur.blurring(broken, alpha, steps, pause, 'd')  #Send to blur function to do the inpainting
+plt.ion()
+data = plt.imshow(broken, plt.cm.gray)
+data.set_array(broken)
+plt.draw()
+plt.pause(1000)
